68a5c4edc0823642114ad0adc99d08278422b260
Andrii Dudar <andrij.dudar@gmail.com>
2023-10-25 16:24:24 +0300

    user

--- src/components/CocktailList.jsx
+++ src/components/CocktailList.jsx
@@ -8,66 +8,71 @@ export function CocktailsList({ cocktailList, onDeleteCocktail }) {
 
       <ol>
         {cocktailList.map(cocktail => (
-            <li key={cocktail.id} className='coctailList'>
-              {/* <div className='coctailList__items'> */}
+          <li key={cocktail.id} className='coctailList'>
             <details className='btns coctailList__items'>
-              <summary className='is-flex coctailList__items'>
-                    <h3 className='coctailList__item coctail_name'>{cocktail.nameCocktail}</h3>
-                    <h3 className='coctailList__item'>&nbsp; &#8680; &nbsp;{cocktail.user}</h3>
-                  </summary>
-                  <ul>
-                    <li className='is-flex is-justify-content-space-between	' style={{marginTop: '20px'}}>
-                      <div>Приготування&nbsp;</div>
-                      <div>{cocktail.cocktailPreparationMethod}</div>
+              <summary className='is-flex'>
+                <h3 className='coctailList__item coctail_name'>{cocktail.nameCocktail}</h3>
+                {/* <h3 className='coctailList__item'>&nbsp; &#8680; &nbsp;{cocktail.user}</h3> */}
+              </summary>
+              <ul>
+                <li className='is-flex is-justify-content-space-between	' style={{ marginTop: '20px' }}>
+                  <div>Автор </div>
+                  <div>{cocktail.user}</div>
+                </li>
+                <hr />
+
+                <li className='is-flex is-justify-content-space-between'>
+                  <div>Приготування&nbsp;</div>
+                  <div>{cocktail.cocktailPreparationMethod}</div>
+                </li>
+
+                <hr />
+
+                <li className='is-flex is-justify-content-space-between	'>
+                  <div>Бокал&nbsp;</div>
+                  <div>{cocktail.glass}</div>
+                </li>
+
+                <hr />
+
+                <li className='is-flex is-justify-content-space-between	'>
+                  <div>Лід</div>
+                  <div>{cocktail.ice}</div>
+                </li>
+
+                <hr />
+
+                <li className='is-flex is-justify-content-space-between	'>
+                  Інгредієнти:
+                </li>
+                <hr />
+                {cocktail.ingredients.map(ingredient => (
+                  <div>
+                    <li key={ingredient.ingredient} className='is-flex is-justify-content-space-between	'>
+                      <div>{ingredient.ingredient}</div>
+                      <div>{ingredient.amount} мл</div>
                     </li>
-
-                    <hr />
-
-                    <li className='is-flex is-justify-content-space-between	'>
-                      <div>Бокал&nbsp;</div>
-                      <div>{cocktail.glass}</div>
-                    </li>
-
-                    <hr />
-
-                    <li className='is-flex is-justify-content-space-between	'>
-                      <div>Лід</div>
-                      <div>{cocktail.ice}</div>
-                    </li>
-
-                    <hr />
-
-                    <li className='is-flex is-justify-content-space-between	'>
-                      Інгредієнти:
-                    </li>
-                    <hr/>
-                    {cocktail.ingredients.map(ingredient => (
-                      <div>
-                        <li key={ingredient.ingredient} className='is-flex is-justify-content-space-between	'>
-                          <div>{ingredient.ingredient}</div>
-                          <div>{ingredient.amount} мл</div>
-                        </li>
-                      </div>
-                    ))}
-                    <hr />
-                    <li className='is-flex is-justify-content-space-between	'>
-                      Опис:
-                    <br/>
-                      {cocktail.description}
-                    </li>
-                    <div className="btn_container">
-                      <button
-                        type="button"
-                        className='btn_dell btn btn_dell-coctail'
-                        onClick={() => onDeleteCocktail(cocktail.id)}
-                      >
-                        Видалити коктейль
-                      </button>
-                    </div>
-                  </ul>
-                </details>
-              {/* </div> */}
-            </li>
+                  </div>
+                ))}
+                <hr />
+                <li className='is-flex is-justify-content-space-between	'>
+                  Опис:
+                  <br />
+                  {cocktail.description}
+                </li>
+                <div className="btn_container">
+                  <button
+                    type="button"
+                    className='btn_dell btn btn_dell-coctail'
+                    onClick={() => onDeleteCocktail(cocktail.id)}
+                  >
+                    Видалити коктейль
+                  </button>
+                </div>
+              </ul>
+            </details>
+            {/* </div> */}
+          </li>
         ))}
       </ol>
     </div>
