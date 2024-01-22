9acebc2ef0123cb3c017fa9ed4c2a2553d8fef08
Andrii Dudar <andrij.dudar@gmail.com>
2024-01-22 02:42:18 +0200

    openCoctails

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { useState } from 'react';
 import './App.scss';
 import { TestPhoto } from './components/TestPhoot';
 import AppCocktails from './components/cocktails/AppCocktails';
-// import AppCocktails from './components/cocktails/AppCocktails';
 
 function App() {
+  const [cocktails, setCocktails] = useState(false);
   return (
     <div className="App">
       <div className="App-header section ">
         <TestPhoto />
-        <AppCocktails />
+        <button
+          className='btn_done btn_add'
+          onClick={() => setCocktails(!cocktails)}
+        >
+          Open Cocktails
+        </button>
+        {cocktails &&
+          <AppCocktails />
+        }
       </div>
     </div>
   );
